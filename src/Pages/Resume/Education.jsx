e0edd67895ad0fedec993d483c53e4b8bfaf0d65
CseRashed <cserashedul@gmail.com>
2025-04-18 15:36:52 +0600

    create Education route in resume route and also responsived

--- src/Pages/Resume/Education.jsx
+++ src/Pages/Resume/Education.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
 
 export default function Education() {
-  return (
-    <div>
-        
-    <div className="  max-h-[70vh] text-white p-4 overflow-y-auto">
+    return (
+        <div>
+
+            <div className="flex gap-5 flex-col lg:flex-row justify-around  max-h-[70vh] text-white p-4 overflow-y-auto">
+<div className='flex gap-6'>
+    <div className='relative'>
+<div className='absolute h-[15px] bg-gradient-to-b from-[#00baf3] to-[#913939] w-[15px] border rounded-full'>
 
 </div>
+<div className="h-[100px] w-[2px] absolute top-3.5 left-1.5 bg-gradient-to-b from-[#00baf3] to-[#913939]"></div>
 
 
     </div>
-  )
+<div className='border space-y-4 p-4 rounded-xl text-center'>
+    <h1 className='lg:text-3xl text-2xl font-medium text-[#1fcaff] '>Secondary School Certificate </h1>
+    <h2 className='font-bold text-[#687d84]'>Osmanpur High School</h2>
+    <p>2016-2021</p>
+</div>
+</div>
+<div className='flex gap-6'>
+    <div className='relative'>
+<div className='absolute h-[15px] bg-gradient-to-b from-[#00baf3] to-[#913939] w-[15px] border rounded-full'>
+
+</div>
+<div className="h-[100px] w-[2px] absolute top-3.5 left-1.5 bg-gradient-to-b from-[#00baf3] to-[#913939]"></div>
+
+
+    </div>
+<div className='border space-y-4 p-4 rounded-xl text-center'>
+    <h1 className='lg:text-3xl text-2xl font-medium text-[#1fcaff] '>Diploma in Computer Science </h1>
+    <h2 className='font-bold text-[#687d84]'>Sherpur Polytechnic Institute</h2>
+    <p>Present</p>
+</div>
+</div>
+
+            </div>
+
+
+        </div>
+    )
 }
